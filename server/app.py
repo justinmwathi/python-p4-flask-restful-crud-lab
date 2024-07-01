49ea8112fc115b889b7c7ce5b60cad63096df1e7
justin <justinmwathi32@gmail.com>
2024-07-01 22:38:41 +0300

    Lab done!

--- server/app.py
+++ server/app.py
@@ -46,7 +46,29 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    def patch(self,id):
+        plant = Plant.query.filter_by(id=id).first()
 
+        if plant is None:
+
+            return jsonify({"error": "Plant not found"}), 404
+
+        data = request.get_json()
+
+        if "is_in_stock" in data:
+
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+
+        return jsonify(plant.to_dict())
+
+    def delete(self,id):
+        plant=Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        response=make_response("",204)
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
